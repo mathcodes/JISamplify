d331fd6f61b99950bec9232b15b3d9d1890297ea
Jon Christie <fullstack11235@gmail.com>
2021-02-04 14:03:42 -0500

    AWS backend complete

--- src/components/CardTitleText/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ArticleContext from "../../utils/ArticleContext";
-
-function CardTitleText() {
-  return <ArticleContext.Consumer>{({ title }) => <h2>{title}</h2>}</ArticleContext.Consumer>;
-}
-
-export default CardTitleText;
